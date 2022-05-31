a98e7e90e6d032607231d08044c156a8f0aee801
Enrique Alvarado <ealvaradoc@gmail.com>
2022-05-31 11:49:46 -0700

    tkinter widgets

--- src/demotk.py
+++ src/demotk.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from re import T
 import numpy as np 
 import tkinter as tk
 from tkinter import *
@@ -8,13 +9,12 @@ from tkinter import messagebox
 # Import the MarkovChain class from markovchain.py
 from markovchain import MarkovChain
 
+
 def drawModel():
     #M = np.column_stack(rows)
     #mc = MarkovChain(M, ['A', 'B', 'C', 'D'])
     #mc.draw()
     messagebox.showinfo('Message', 'You clicked the Submit button!')
-    #return 0
-
 
 root = Tk()  
 root.wm_geometry("800x600+0+0")
@@ -22,23 +22,34 @@ root.wm_title("UP to 4x4 Markov Chain Draw")
 root.focus_set()
 root.grab_set()
 
+row_col = StringVar()
+row_col = "3"
+
+def callback(row_col):
+    print (row_col)
+
 myFont = tkinter.font.Font(family = 'Helvetica' , size = 12, weight = 'bold')
 win0 = Frame(root)
 win0.pack(side="top", fill="both", expand=True)
-label0 = Label(win0, text="Enter the transition matrix:", font=myFont)
-
+label0 = Label(win0, text="Draw MDP from transition Matrix", font=myFont)
+label0.pack(side="top", fill="both", expand=True)
 win1 = Frame(root)
 win1.pack(side="top", fill="both", expand=True)
-label1 = Label(win1, text="Enter the transition matrix:", font=myFont)
-label1.pack()
 photo = PhotoImage(file = "markov.png")
 w = Label(win1, image=photo)
 w.pack()
-
+lblRows = Label(win1, text="Rows & cols :", font=myFont)
+lblRows.pack(side=TOP)
+#row_col.trace("w", lambda name, index, mode, row_col=row_col: callback(row_col))
+e1 = Entry(win1, bd =5, width=10, textvariable=row_col, font=myFont)
+e1.insert(0,row_col)
+e1.pack(side=TOP)
+label1 = Label(win1, text="Transition Matrix :", font=myFont)
+label1.pack(side=TOP)
 
 frame1 = Frame(win1)
-for j in range(4):
-    for i in range(4):
+for j in range(int(row_col)):
+    for i in range(int(row_col)):
         e = Entry(frame1,width=10,font=myFont,bd=5)
         e.grid(row=i, column=j, sticky=NSEW)
         frame1.pack()
